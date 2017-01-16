4826affeefb4f76d4cd25cea7faaf741db8799b0
Ivan Chan <29january@gmail.com>
2017-01-17 00:11:19 +0800

    added producttype service and model.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ProducttypeComponentComponent } from './producttype-component/producttype-component.component';
 import { ProducttypeComponent } from './component/producttype/producttype.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProducttypeComponentComponent,
     ProducttypeComponent
   ],
   imports: [
