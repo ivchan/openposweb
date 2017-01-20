9bc6c4ca6d1476c7b20d60520dee9d0ff878a030
Ivan Chan <29january@gmail.com>
2017-01-20 21:35:22 +0800

    updated customer list, add , edit page.

--- src/app/component/customer-add/customer-add.component.ts
+++ src/app/component/customer-add/customer-add.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {ICustomer} from "../../model/customer-model";
+import {SharedService} from "../../service/sharedservice-service";
+import {CustomerService} from "../../service/customer-service";
 
 @Component({
   selector: 'app-customer-add',
   templateUrl: 'customer-add.component.html',
-  styleUrls: ['customer-add.component.css']
+  styleUrls: ['customer-add.component.css'],
+  providers: [SharedService, CustomerService]
 })
 export class CustomerAddComponent implements OnInit {
+  public customer: ICustomer;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _sharedService: SharedService, private _customerService: CustomerService) {
   }
 
+  ngOnInit() {
+    this.customer = <ICustomer>{};
+    this.customer.customerCode = "ivanchan";
+    this.customer.firstName = "Ivan";
+    this.customer.lastName = "Chan";
+  }
+
+  public saveCustomer(): void {
+    alert( JSON.stringify(this.customer) );
+  }
 }
